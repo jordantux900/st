006a7beb49b129eaa93682727726fe78a2d857fb
jordantux900 <aelskulra@gmail.com>
2024-09-03 16:25:55 +0200

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mononoki:pixelsize=11:antialias=true:autohint=true";
+static char *font = "Mononoki Nerd Font:pixelsize=11:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
